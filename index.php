bc3e0e3892363a5380c99e159ac65b4162787ffa
Marcelo Joia <sitejoia@hotmail.com>
2023-09-06 23:45:56 -0300

    Os Blocos Básicos de Criação do Tema (parte 1)

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fany Labs</title>
 </head>
-<body>
-    <p>Apenas um teste</p>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+    <div id="page" class="site">
+        <header>
+            <section class="search">Pesquisa</section>
+            <section class="top-bar">
+                <div class="brand"> Logo</div>
+
+                <div id="second-column">
+                    <div class="acount">Conta</div>
+                    <nave class="main-menu">Menu</nave>
+                </div>
+
+            </section>
+        </header>
+
+        <div class="content-area">
+            <main>
+                <section class="slider">Slider</section>
+                <section class="popular-product">Produto popular</section>
+                <section class="new-arrivals">Lançamentos</section>
+                <section class="deal-of-the-week">Promoção</section>
+                <section class="lab-blog">Notícias</section>
+            </main>
+        </div>
+    </div>
+
+    <div class="footer-widgets"></div>
+    <div class="copyright">Copyright</div>
 </body>
+
 </html>
