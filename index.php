1581aa4fa1e012cb488f8eea053462a6659b5b63
Marcelo Joia <sitejoia@hotmail.com>
2023-09-09 01:14:05 -0300

    O Loop WordPress

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -12,35 +13,54 @@
  * @package Fancy Lab
  */
 
-get_header(); 
+get_header();
 ?>
-		<div class="content-area">
-			<main>
-				<section class="slider">
-					<div class="container">
-						<div class="row">Slider</div>
-					</div>
-				</section>
-				<section class="popular-products">
-					<div class="container">
-						<div class="row">Produtos populares</div>
-					</div>
-				</section>
-				<section class="new-arrivals">
-					<div class="container">
-						<div class="row">Lançamentos</div>
-					</div>
-				</section>
-				<section class="deal-of-the-week">
-					<div class="container">
-						<div class="row">Promoção da semana</div>
-					</div>
-				</section>
-				<section class="lab-blog">
-					<div class="container">
-						<div class="row">Notícias</div>
-					</div>
-				</section>
-			</main>
-		</div>
+<div class="content-area">
+    <main>
+        <section class="slider">
+            <div class="container">
+                <div class="row">Slider</div>
+            </div>
+        </section>
+        <section class="popular-products">
+            <div class="container">
+                <div class="row">Produtos populares</div>
+            </div>
+        </section>
+        <section class="new-arrivals">
+            <div class="container">
+                <div class="row">Lançamentos</div>
+            </div>
+        </section>
+        <section class="deal-of-the-week">
+            <div class="container">
+                <div class="row">Promoção da semana</div>
+            </div>
+        </section>
+        <section class="lab-blog">
+            <div class="container">
+                <div class="row">
+
+                    <?php if (have_posts()) : ?>
+
+                        <?php while (have_posts()) : ?>
+                            <?php the_post(); ?>
+
+                            <article>
+                                <h2><?php the_title(); ?></h2>
+                                <div><?php the_content(); ?></div>
+                            </article>
+
+
+                        <?php endwhile; ?>
+
+                    <?php else : ?>
+                        <p>Nothing the display</p>
+                    <?php endif; ?>
+
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
 <?php get_footer(); ?>
