3b76c5fb96ac82915db028e053fd83260e636a6a
Marcelo Joia <sitejoia@hotmail.com>
2023-09-09 00:49:08 -0300

    Ajuste para o tema da aula

--- header.php
+++ header.php
@@ -1,53 +1,46 @@
 <?php
-
 /**
- * The header for our theme.
+ * The header for our theme
  *
  * This is the template that displays all of the <head> section and everything up until <header>
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Fancy Labs
- */ ?>
-<!DOCTYPE html>
+ * @package Fancy Lab
+ */
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Tudo que for enfileirado será colocado na tag [wp_head()] do Theme -->
-    <?php wp_head(); ?>
-
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-
-    <div id="page" class="site">
-        <header>
-
-            <!-- Search Inicio -->
-            <section class="search">
-                <div class="container">
-                    Pesquisa
-                </div>
-            </section>
-            <!-- Search FIM -->
-
-            <section class="top-bar">
-
-                <div class="container">
-                    <div class="row">
-                        <div class="brand col-3">
-                            Logo
-                        </div>
-                        <div class="second-column col-9">
-                            <div class="acount">Conta</div>
-                            <nav class="main-menu"><?php wp_nav_menu(array('fancy_lab_main_menu')); ?></nav>
-                        </div>
-                    </div>
-                </div>
-
-            </section>
-
-        </header>
+	<div id="page" class="site">
+		<header>
+			<section class="search">
+				<div class="container">
+					Pesquisa
+				</div>
+			</section>
+			<section class="top-bar">
+				<div class="container">
+					<div class="row">
+						<div class="brand col-3">Logo</div>
+						<div class="second-column col-9">
+							<div class="account">Conta</div>
+							<nav class="main-menu">
+								<?php 
+									wp_nav_menu(
+										array(
+											'theme_location'	=> 'fancy_lab_main_menu'
+										)
+									); 
+								?>
+							</nav>
+						</div>						
+					</div>
+				</div>
+			</section>
+		</header>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The main template file
  *
@@ -10,18 +9,38 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package Fancy Labs
+ * @package Fancy Lab
  */
 
-get_header(); ?>
-
-        <div class="content-area">
-            <main>
-                <section class="slider">Slider</section>
-                <section class="popular-product">Produto popular</section>
-                <section class="new-arrivals">Lançamentos</section>
-                <section class="deal-of-the-week">Promoção</section>
-                <section class="lab-blog">Notícias</section>
-            </main>
-        </div>
-        <?php get_footer(); ?>
+get_header(); 
+?>
+		<div class="content-area">
+			<main>
+				<section class="slider">
+					<div class="container">
+						<div class="row">Slider</div>
+					</div>
+				</section>
+				<section class="popular-products">
+					<div class="container">
+						<div class="row">Produtos populares</div>
+					</div>
+				</section>
+				<section class="new-arrivals">
+					<div class="container">
+						<div class="row">Lançamentos</div>
+					</div>
+				</section>
+				<section class="deal-of-the-week">
+					<div class="container">
+						<div class="row">Promoção da semana</div>
+					</div>
+				</section>
+				<section class="lab-blog">
+					<div class="container">
+						<div class="row">Notícias</div>
+					</div>
+				</section>
+			</main>
+		</div>
+<?php get_footer(); ?>
